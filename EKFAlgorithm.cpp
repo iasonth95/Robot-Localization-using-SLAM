c7cc4e7ec3826758be2ed60b65cb768f8c006519
Spyros <spyros@programmer.net>
2024-06-15 19:34:01 +0300

    Restructure the project

--- EKFAlgorithm.cpp
+++
@@ -1,8 +0,0 @@
-#include "EKFAlgorithm.h"
-#include <iostream>
-
-EKFAlgorithm::EKFAlgorithm() {}
-
-void EKFAlgorithm::run(std::vector<Robot>& Robots, std::vector<Landmark>& Landmark_Groundtruth, double timesteps, double sample_time) {
-    std::cout << "Running EKF algorithm for " << Robots.size() << " robots with " << Landmark_Groundtruth.size() << " landmarks at time " << timesteps << " with sample time " << sample_time << "..." << std::endl;
-}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+# Configure
+cmake -Bbuild -GNinja
+# Build
+cmake --build build
+#Run 
+./src/localization
 coming soon....
